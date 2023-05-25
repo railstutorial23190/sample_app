6d655a57696c07c65e6673440da8bfb9945eea91
Ubuntu <ubuntu@ip-172-31-44-194.ap-northeast-1.compute.internal>
2023-05-25 06:40:54 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/signup', to:"users#new"
+  
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/contact', to: 'static_pages#contact'
+  get '/about',   to: 'static_pages#about'
+  get '/help',    to: 'static_pages#help'
 end
